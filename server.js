3dcb54336ef1cbe9cf6f551e02e962dbc9087bdf
Manish <manishwebworks@gmail.com>
2025-05-02 22:54:29 +0530

    server running

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ const userRoutes = require('./routes/user');
 // Load environment variables
 dotenv.config();
 
+// Connect to database
+connectDB().then(() => {
+    console.log('Database connected successfully');
+}).catch(err => {
+    console.error('Database connection failed:', err);
+    process.exit(1);
+});
+
 // Constants
 const {
     PORT = 5000,
